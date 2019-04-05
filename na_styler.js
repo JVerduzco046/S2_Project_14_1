f7c8045401a4a55263535992a22c6a883ed30077
Juan Verduzco <jverduzco046@west-mec.org>
2019-04-04 17:26:20 -0700

    Final Commit
    Ready For  Review

---
+++ na_styler.js
@@ -0,0 +1,90 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Case Problem 1
+
+   Author: Juan Verduzco
+   Date:   04.02.19
+
+   Filename: na_styler.js
+
+   Functions
+   =========
+   
+   setStyles()
+      Sets up the style sheets and the style sheet switcher.
+      
+   randInt(size)
+      Returns a random integer from 0 up to size-1.
+
+*/
+
+function randInt(size) {
+
+      return Math.floor(size * Math.random());
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+window.addEventListener("load", setStyles);
+
+function setStyles() {
+
+      var styleNum = randInt(5);
+
+      var fancySheet = document.createElement("link");
+
+      fancySheet.setAttribute("rel", "stylesheet");
+
+      fancySheet.setAttribute("id", "fancySheet");
+
+      fancySheet.setAttribute("href", "na_style_" + styleNum + ".css")
+
+      document.head.appendChild(fancySheet);
+
+      var figBox = document.createElement("figure");
+
+      figBox.setAttribute("id", "styleThumbs");
+      //appensds the element "box" to figBox
+      document.getElementById("box").appendChild(figBox);
+
+      for (let i = 0; i <= 4; i++) {
+
+            var sheetImg = document.createElement("img");
+
+            sheetImg.setAttribute("src", "na_small_" + i + ".png");
+
+            sheetImg.setAttribute("alt", "na_style_" + i + ".css");
+
+            sheetImg.addEventListener("click", function (e) {
+
+                  document.getElementById("fancySheet").setAttribute("href", e.target.alt);
+            });
+            figBox.appendChild(sheetImg);
+      }
+      var thumbStyles = document.createElement("style");
+
+      document.head.appendChild(thumbStyles);
+
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs { \
+                  position:absolute; \
+                  left: 0px; \
+                  bottom: 0px; \
+            }", 0);
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs img { \
+            outline: 1px solid black; \
+            cursor: pointer; \
+            opacity: 0.75; \
+      }", 1);
+
+
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+
+            "figure#styleThumbs img:hover { \
+            outline: 1px solid red; \
+            opacity: 1.0; \
+      }", 2);
+}
